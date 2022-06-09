4cff98a45eb167a65e80ef1560abb436389814c0
Main Menu <corigan01@gmail.com>
2022-06-08 23:24:09 -0500

    drawing pixels and rec now possible!
    The ability to draw colored pixels "safely" is not implemented. This involves getting a `framebuffer` from the bootloader and passing it to the custom framebuffer (`FBuffer`) struct that has impl functions that have the ability to draw rectangles and pixels depending on if the correct information is provided.
    
    ### Example Implementation
    ---
    ```rust
    let buffer = FBuffer::new(&boot_info.framebuffer);
    
    // Lets draw a rectangle at pos x = 0, and y = 0 with a size of x + 10, y + 10
    //                pos     size     COLOR
    //              (x, y), (x+, y+)  HEX CODE
    buffer.draw_rec((0, 0), (10, 10), 0xFFFFFF);
    ```

--- src/vga/mod.rs
+++ src/vga/mod.rs
@@ -5,42 +5,35 @@
 \___\_\_,_/\_,_/_//_/\__/\_,_/_/_/_/ /_/|_|\__/_/ /_//_/\__/_/
   Part of the Quantum OS Kernel
 
- VGA TEXT MODE DRIVER
- =======================
+Copyright 2022 Gavin Kellam
 
- This file is here to contain the vga text mode driver functions such as drawing to the screen.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
-use volatile::Volatile;
-
 pub mod low_level;
 
-pub enum FramebufferType {
-    TextMode,
-    PixelMode
-}
+use bootloader::boot_info::{FrameBuffer, FrameBufferInfo};
 
-pub enum OutputType {
-    TextOnly,
-    PixelOnly
-}
+pub fn init_kernel_vga(framebuffer: FrameBuffer) {
+    let framebuffer_info = framebuffer.info();
+    let framebuffer_pixel = framebuffer_info.pixel_format;
 
-pub fn init_kernel_vga(framebuffer_type: FramebufferType, output_type: OutputType,
-                        buffer: u64, frame_size: (u32, u32)) {
 
-    match framebuffer_type {
-        TextMode => {
 
-        },
-
-        PixelMode => {
-
-        },
-
-        _ => {
-
-        }
-    }
 
 }
+
