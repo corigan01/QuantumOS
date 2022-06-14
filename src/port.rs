2216c791864276847f2ffd319f805d818ca92407
Main Menu <corigan01@gmail.com>
2022-06-14 16:00:09 -0500

    Added Docs to `port.rs` and added custom impl for `serial.rs`

---
+++ src/port.rs
@@ -0,0 +1,149 @@
+/*!
+```
+  ____                 __               __ __                 __
+ / __ \__ _____ ____  / /___ ____ _    / //_/__ _______  ___ / /
+/ /_/ / // / _ `/ _ \/ __/ // /  ' \  / ,< / -_) __/ _ \/ -_) /
+\___\_\_,_/\_,_/_//_/\__/\_,_/_/_/_/ /_/|_|\__/_/ /_//_/\__/_/
+  Part of the Quantum OS Kernel
+
+Copyright 2022 Gavin Kellam
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
+
+# Quantum OS Ports
+
+according to the OSDEV Wiki --- https://wiki.osdev.org/I/O_Ports
+An I/O port is usually used as a technical term for a specific address on the x86's IO bus. This
+bus provides communication with devices in a fixed order and size, and was used as an alternative
+to memory access. On many other architectures, there is no predefined bus for such communication
+and all communication with hardware is done via memory-mapped IO. This also increasingly happens
+on modern x86 hardware.
+
+
+## Why would anyone need to use these?
+Old cpu I/O ports are still used by some critical system components like the
+PIT (Programmable Interrupt Timer) and or the PIC (Programmable Interrupt Controller) among others.
+
+However, these functions should only be used when cpu IO is needed! CPU I/O is slow and can cause
+issues on some hardware because of how legacy devices communicate with the CPU's I/O bus. These
+functions are considered unsafe because of the unknown harm some I/O ports could cause on the memory
+of kernel. Most times everything you do with CPU I/O ports should be in a wrapper function. That
+would assure the operation is safe and checks input and outputs.
+
+
+## List of IO Ports commonly used (https://wiki.osdev.org/I/O_Ports)
+```
+Port range	    Summary
+0x0000-0x001F	The first legacy DMA controller
+0x0020-0x0021	The first PIC
+0x0022-0x0023	Access to the Model-Specific Registers of Cyrix processors.
+0x0040-0x0047	The PIT
+0x0060-0x0064	The "8042" PS/2 Controller or its predecessors, dealing with keyboards and mice.
+0x0070-0x0071	The CMOS and RTC registers
+0x0080-0x008F	The DMA (Page registers)
+0x0092	        The location of the fast A20 gate register
+0x00A0-0x00A1	The second PIC
+0x00C0-0x00DF	The second DMA controller, often used for SoundBlasters
+0x00E9	        Home of the Port E9 Hack. Used on some emulators to directly send text to the hosts' console.
+0x0170-0x0177	The secondary ATA hard disk controller.
+0x01F0-0x01F7	The primary ATA hard disk controller.
+0x0278-0x027A	Parallel port
+0x02F8-0x02FF	Second serial port
+0x03B0-0x03DF	The range used for the IBM VGA, its direct predecessors, as well as any modern video card in legacy mode.
+0x03F0-0x03F7	Floppy disk controller
+0x03F8-0x03FF	First serial port
+```
+*/
+
+use core::arch::asm;
+
+
+/// # byte_in
+///
+/// ### Operation
+/// Returns *1* Byte from the CPU I/O bus!
+///
+/// ### Safety
+/// This function directly preforms the CPU instruction to interact with the basic CPU I/O
+/// registers. Which means it has no protection or assured memory safety. This function preforms the
+/// instruction `in` in x86_64 asm which directly interfaces with the CPU's I/O bus.
+///
+/// This function is unsafe! Please use a wrapper to assure memory safety whenever possible.
+#[inline]
+pub unsafe fn byte_in(port: u16) -> u8 {
+    let mut port_value: u8 = 0;
+
+    asm!("in al, dx", out("al") port_value, in("dx") port, options(nomem, nostack, preserves_flags));
+
+    port_value
+}
+
+/// # byte_out
+///
+/// ### Operation
+/// Puts *1* Byte onto the CPU I/O bus!
+///
+/// ### Safety
+/// This function directly preforms the CPU instruction to interact with the basic CPU I/O
+/// registers. Which means it has no protection or assured memory safety. This function preforms the
+/// instruction `out` in x86_64 asm which directly interfaces with the CPU's I/O bus.
+///
+/// This function is unsafe! Please use a wrapper to assure memory safety whenever possible.
+#[inline]
+pub unsafe fn byte_out(port: u16, data: u8) {
+    asm!("out dx, al", in("dx") port, in("al") data, options(nomem, nostack, preserves_flags));
+}
+
+
+/// # word_in
+///
+/// ### Operation
+/// Returns *2* Bytes from the CPU I/O bus!
+///
+/// ### Safety
+/// This function directly preforms the CPU instruction to interact with the basic CPU I/O
+/// registers. Which means it has no protection or assured memory safety. This function preforms the
+/// instruction `in` in x86_64 asm which directly interfaces with the CPU's I/O bus.
+///
+/// This function is unsafe! Please use a wrapper to assure memory safety whenever possible.
+#[inline]
+pub unsafe fn word_in(port: u16) -> u16 {
+    let mut port_value: u16 = 0;
+
+    asm!("in ax, dx", out("ax") port_value, in("dx") port, options(nomem, nostack, preserves_flags));
+
+    port_value
+}
+
+
+/// # word_out
+///
+/// ### Operation
+/// Puts *2* Bytes onto the CPU I/O bus!
+///
+/// ### Safety
+/// This function directly preforms the CPU instruction to interact with the basic CPU I/O
+/// registers. Which means it has no protection or assured memory safety. This function preforms the
+/// instruction `out` in x86_64 asm which directly interfaces with the CPU's I/O bus.
+///
+/// This function is unsafe! Please use a wrapper to assure memory safety whenever possible.
+#[inline]
+pub unsafe fn word_out(port: u16, data: u16) {
+    asm!("out dx, ax", in("dx") port, in("ax") data, options(nomem, nostack, preserves_flags));
+}
+
+
--- src/serial.rs
+++ src/serial.rs
@@ -1,19 +1,127 @@
-use uart_16550::SerialPort;
-use spin::Mutex;
+use crate::port;
+use crate::port::{byte_in, byte_out};
+use crate::serial::SerialCOM::COM_1;
 use lazy_static::lazy_static;
+use spin::Mutex;
+use core::fmt;
+use core::fmt::Arguments;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum SerialCOM {
+    COM_1 = 0x3F8,
+    COM_2 = 0x2F8,
+    COM_3 = 0x3E8,
+    COM_4 = 0x2E8,
+    COM_NULL = 0x00
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SerialDevice {
+    port: SerialCOM
+}
+
+impl SerialDevice {
+
+    // Constructs a new SerialDevice
+    // This function will return a `Option<T>` in the event that
+    pub fn new(port: SerialCOM) -> Option<SerialDevice> {
+        let mode = port as u16;
+
+        unsafe {
+            /*
+                | IO |  D  |  	Register mapped to this port
+                |----|-----|----------------------------------------------------
+                | +0 |	0  |	Data register. Reading this registers read from the Receive buffer. Writing to this register writes to the Transmit buffer.
+                | +1 |	0  |	Interrupt Enable Register.
+                | +0 |	1  |	With DLAB set to 1, this is the least significant byte of the divisor value for setting the baud rate.
+                | +1 |	1  |	With DLAB set to 1, this is the most significant byte of the divisor value.
+                | +2 |	-  |	Interrupt Identification and FIFO control registers
+                | +3 |	-  |	Line Control Register. The most significant bit of this register is the DLAB.
+                | +4 |	-  |	Modem Control Register.
+                | +5 |	-  |	Line Status Register.
+                | +6 |	-  |	Modem Status Register.
+                | +7 |	-  |	Scratch Register.
+
+                (https://wiki.osdev.org/Serial_Ports)
+            */
+
+            // INIT
+            port::byte_out(mode + 1, 0x00); // Disable the Serial Port interrupts
+            port::byte_out(mode + 3, 0x80); // Enable DLAB which sets the baud rate divisor
+            port::byte_out(mode + 0, 0x03); // Set the baud rate to 38400 (lo byte)
+            port::byte_out(mode + 1, 0x00); //                            (hi byte)
+            port::byte_out(mode + 3, 0x03); // Use 8 bits, no parity bits, and one stop bit
+            port::byte_out(mode + 2, 0xC7); // Enable FIFO
+            port::byte_out(mode + 4, 0x1E); // Set Serial to loopback mode
+
+            // TEST
+            let test_byte: u8 = b'A';
+
+            port::byte_out(mode + 0, test_byte); // Send test byte
+
+            let serial_response = port::byte_in(mode + 0);
+
+            if serial_response != test_byte {
+                return None;
+            }
+
+            // Success
+            port::byte_out(mode + 4, 0x0F);
+        }
+
+        Some(SerialDevice { port })
+
+    }
+
+    unsafe fn is_transmit_empty(&self) -> bool {
+        port::byte_in(self.port as u16 + 5) & 0x20 != 0x00
+    }
+
+    pub fn write_byte(&self, byte: u8) {
+        assert_ne!(self.port, SerialCOM::COM_NULL);
+
+        unsafe {
+            loop {
+              if self.is_transmit_empty() {
+                  break;
+              }
+            }
+
+            port::byte_out(self.port as u16, byte);
+        }
+    }
+
+    pub fn write_string(&self, string: &str) {
+        for byte in string.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe)
+            }
+        }
+    }
+}
+
+impl fmt::Write for SerialDevice {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
 
 lazy_static! {
-    pub static ref SERIAL1: Mutex<SerialPort> = {
-        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
-        serial_port.init();
-        Mutex::new(serial_port)
-    };
+    pub static ref SERIAL1: Mutex<Option<SerialDevice>> =
+        Mutex::new(SerialDevice::new(SerialCOM::COM_2));
 }
+
 
 #[doc(hidden)]
 pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
-    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed");
+    if SERIAL1.lock().as_ref() != None {
+        SERIAL1.lock().unwrap().write_fmt(args).unwrap();
+    }
 }
 
 /// Prints to the host through the serial interface.
