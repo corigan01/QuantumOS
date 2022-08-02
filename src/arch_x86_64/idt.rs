b2c2dbc7c3ee279bd04bf7e8cbf2622fa42de0ce
Main Menu <corigan01@gmail.com>
2022-08-02 17:38:27 -0500

    Added Documentation to IDT to aid with development

--- src/arch_x86_64/idt.rs
+++ src/arch_x86_64/idt.rs
@@ -34,7 +34,6 @@ use x86_64::structures::gdt::SegmentSelector;
 use x86_64::PrivilegeLevel;
 use x86_64::PrivilegeLevel::Ring0;
 
-
 type RawHandlerFuncNe  /* No Error             */ = extern "x86-interrupt" fn(InterruptFrame);
 type RawHandlerFuncE   /* With Error           */ = extern "x86-interrupt" fn(InterruptFrame, u64);
 type RawHandlerFuncDne /* Diverging No Error   */ = extern "x86-interrupt" fn(InterruptFrame) -> !;
@@ -47,9 +46,91 @@ type RawHandlerFuncDe  /* Diverging With Error */ = extern "x86-interrupt" fn(In
 /// filled, but the error not not always be apparent.
 pub type GeneralHandlerFunc = fn(InterruptFrame, u8, Option<u64>);
 
+/// # IDT (interrupt descriptor table)
+/// This is the root component that controls how and when ISRs will run. ISR stands for interrupt
+/// service routines. These are functions that can be called when the cpu has either a fault, or
+/// a software triggered interrupt. Software triggered interrupts can be used for several things
+/// including 'System Calls'. System calls can be used to ask the kernel to perform privileged
+/// operations like talking to I/O in a safe way, as to not allow the sand boxed application to
+/// have privileges to things it shouldn't be.
+///
+/// # How to setup a basic IDT
+/// IDT can be setup in the following way
+/// ```rust
+/// use quantum_os::arch_x86_64::idt::Idt;
+/// use quantum_os::arch_x86_64::idt::InterruptFrame;
+/// use quantum_os::{attach_interrupt, serial_println};
+/// use lazy_static::lazy_static;
+///
+/// // This is the handler that gets called whenever `#DE` is called
+/// fn divide_by_zero_handler(i_frame: InterruptFrame, int_n: u8, error_code: Option<u64>) {
+///     serial_println!("EXCEPTION: DIVIDE BY ZERO {}", int_n);
+/// }
+///
+/// // We want our IDT to be in a lazy_static to make sure it has infinite lifetime
+/// lazy_static! {
+///     static ref IDT: idt::Idt = {
+///         let mut idt = idt::Idt::new();
+///
+///         // This is where we tell our IDT that we want our `divide_by_zero_handler` handler to
+///         // be called whenever interrupt 0 (#DE) is called.
+///         attach_interrupt!(idt, divide_by_zero_handler, 0);
+///
+///         idt
+///     };
+/// }
+///
+/// fn load_idt_into_memory() {
+///
+///     // To load our IDT we need to first `.submit_entries()`, which will turn the IDT into the
+///     // more primitive structure that the CPU can understand. This structure is only generated
+///     // from this command, and for safety there is no other way to make it. However, you might
+///     // have noticed that there is an `.unwrap()` after `.submit_entries()`. The method that
+///     // turns the IDT into the correct structure *might* fail if there is a malformed entry.
+///     // So in this case we need to make sure we have a completely valid structure before we
+///     // can use the `.load()`.
+///     IDT.submit_entries().unwrap().load();
+/// }
+///
+/// ```
 pub struct Idt([Entry; 255]);
 
 
+/// # Entry
+/// This is the most basic form of the IDT. Each entry is made up of a few parts that the cpu needs
+/// in a very specific order to understand. This order is as follows
+/// ```text
+///  pointer_low: u16
+///  gdt_selector: SegmentSelector
+///  options: EntryOptions
+///  pointer_middle: u16
+///  pointer_high: u32
+///  reserved: u32
+/// ```
+/// The first 2-bytes of the structure are the low bytes of the pointer. This is not the entire
+/// pointer, but only the first 16 bits of it. Since X86 is a weird architecture, it has odd
+/// backwards compatibility, because of this we have the pointer split-up.
+///
+/// The `gdt_selector` is simple little structure that gives a GDT reference to every entry. This
+/// mostly does not matter in 64-bit mode, but in older versions it was how privileges where controlled.
+///
+/// The `options` is how the cpu knows who, and when can an interrupt be called. For example, a
+/// userspace application is not allowed to call `Double Fault` directly because then any program
+/// can just cause the CPU to panic and the operating system would not be able to shut the program
+/// down. For this reason userspace applications are only allowed to call the interrupts that the
+/// kernel agrees is necessary. These are usually system calls that are allowed to be called. This
+/// is because the operating system has full control on what the interrupts do and dont do.
+///
+/// The next two fields in our Entry struct is the second half of the pointer as mentioned before.
+/// This is just the higher 48-bits of the pointer and the rest are discarded and must be 0.
+///
+/// `reserved` is probably the most odd thing in here. These bits **MUST** be zero. Sometimes
+/// emulators will store information in them, and on real hardware it can cause a Protection Fault
+/// if any of these bits are set. In special cases, these bits can also make the system super
+/// unpredictable. Weird memory or untraceable errors can happen if these bits are set. For that
+/// reason, when you submit the IDT it checks if these bits are zero in every entry and will Err if
+/// there is even a single bit set.
+///
 #[derive(Debug, Clone, Copy)]
 #[repr(C, packed)]
 pub struct Entry {
@@ -61,16 +142,34 @@ pub struct Entry {
     reserved: u32,
 }
 
+
+/// # Interrupt Frame
+/// This is probably the struct most people will need to learn. This struct is very important as it
+/// represents a snapshot of the interrupt that was called. It will tell you things as simple as where
+/// the cpu was executing, this can be very important for debugging. I would suggest printing out
+/// the EIP on almost every fault because it will tell you exactly the instruction that caused the
+/// CPU to fault. As we move on to the flags, these can be kinda specific to the interrupt. I would
+/// suggest looking at the OSdev wiki for more information on the flags for your specific interrupt.
+///
+/// # Notes
+/// This can be one of the most important things for debugging, so please learn exactly how this
+/// structure can be used. It will save so much time knowing exactly what happened instead of looking
+/// for what could have caused this issue in the first place.
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct InterruptFrame {
-    eip: VirtualAddress,
-    code_seg: u64,
-    flags: u64,
-    stack_pointer: VirtualAddress,
-    stack_segment: u64
+    pub eip: VirtualAddress,
+    pub code_seg: u64,
+    pub flags: u64,
+    pub stack_pointer: VirtualAddress,
+    pub stack_segment: u64
 }
 
+
+/// # Missing handler
+/// This handler is here for safety. This is called whenever an interrupt is called, but unfortunately
+/// the user forgot to add a handler for that interrupt. It does not really provide much info, but
+/// it causes the system to crash to make sure undefined behavior or a protection fault occurred.
 #[cfg(not(test))]
 extern "x86-interrupt" fn missing_handler(i_frame: InterruptFrame) -> ! {
     panic!("Missing Interrupt handler was called! Please add a handler to handle this interrupt! {:#x?}", i_frame);
@@ -259,6 +358,22 @@ impl Idt {
     }
 }
 
+/// # IDT Table Pointer
+/// This is the pointer to where you stored your IDT, but in a structure the CPU will accept. This
+/// structure is created when you submit_entries in the IDT. This is the only way of making this
+/// because the IDT controls such fundamental CPU operations, it would be very unsafe for anyone
+/// to make this structure. If this is malformed, it can cause the entire operating system to have
+/// issues that are almost untraceable. Whenever the CPU tries to call an interrupt and this does
+/// not point a valid IDT, it will call a `General Protection Fault`. This fault then isn't handled
+/// (because again the idt doesn't exist) so a `Double Fault` will be called. Once the `Double Fault`
+/// is called, the cpu already knows it can't recover. This isn't the end for our poor cpu, however,
+/// this interrupt isn't handled again, so it causes a `Triple Fault`. This is finally the end for
+/// the cpu. This interrupt can not be handled and the cpu is forced to reset the system. Once the
+/// system is reset, however, the entire process will happen over and over. This is generally considered
+/// "boot looping", because the system will boot and then shutdown over and over.
+///
+/// This is why it is very important that this is made correctly, and for that reason is can only be
+/// made through the IDT.
 #[derive(Copy, Clone, Debug)]
 #[repr(C, packed(2))]
 pub struct IdtTablePointer{
@@ -453,6 +568,8 @@ macro_rules! attach_interrupt {
 
 }
 
+// --- Tests ---
+// These are to ensure the system is working as intended.
 
 #[cfg(test)]
 extern "x86-interrupt" fn missing_handler(i_frame: InterruptFrame) {
