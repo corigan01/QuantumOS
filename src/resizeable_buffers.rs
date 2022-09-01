39c1d302088e81cbed1d09cf5542918a7b416e10
Main Menu <33582457+corigan01@users.noreply.github.com>
2022-08-31 23:45:36 -0500

    bitset.rs fixed and integrated instead of bitfield

--- src/resizeable_buffers.rs
+++ src/resizeable_buffers.rs
@@ -22,49 +22,7 @@ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FO
 DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Quantum OS Lib file, documentation coming soon!
-
 */
 
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
-#![allow(dead_code)]
+use crate::{debug_println, debug_print};
 
-#![cfg_attr(test, no_main)]
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_handler::test_runner)]
-#![feature(abi_x86_interrupt)]
-
-#![reexport_test_harness_main = "run_test"]
-
-#[cfg(test)]
-use bootloader::{BootInfo, entry_point};
-
-#[cfg(test)]
-entry_point!(test_main);
-
-#[cfg(test)]
-/// Entry point for `cargo test`
-fn test_main(boot_info: &'static mut BootInfo) -> ! {
-    serial_println!("\n\n== QuantumOS in Test Mode == \n");
-
-
-    run_test();
-
-    loop {};
-}
-
-
-pub mod test_handler;
-
-pub mod serial;
-pub mod vga;
-pub mod port;
-pub mod arch_x86_64;
-pub mod memory;
-pub mod bitset;
-pub mod panic;
-pub mod qemu;
-pub mod post_hal;
-pub mod debug_output;
-pub mod resizeable_buffers;
