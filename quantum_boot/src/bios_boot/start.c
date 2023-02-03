7814a263a5f1c4b510ffd24a7165fd68db37cc99
corigan01 <corigan01@gmail.com>
2023-02-02 18:11:52 -0600

    files

--- quantum_boot/src/bios_boot/start.c
+++ quantum_boot/src/bios_boot/start.c
@@ -1,4 +1,5 @@
 char* video_memory = (char*) 0xb8000;
+int bytes[4] = {0xAA, 0xBB, 0xCC, 0xDD};
 
 void print_str() {
     for (char* i = video_memory; *i != 0; i++) {
@@ -7,12 +8,9 @@ void print_str() {
 }
 
 void cmain() {
+    asm ("int $0x10");
 
-    asm ("nop");
     for (int i = 0; i < 100; i++) {
-        *(video_memory += 2) = 'X';
+        *(video_memory++) = 'X';
     }
-
-
-    while(1) {};
 }
