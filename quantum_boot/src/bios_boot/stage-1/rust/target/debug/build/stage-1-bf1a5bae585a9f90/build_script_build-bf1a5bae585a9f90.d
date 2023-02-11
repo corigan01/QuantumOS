cfbfafac40fe462e1641464a6250d7e5b88b842e
Main Menu <corigan01@gmail.com>
2023-02-10 21:37:21 -0600

    refactor bootloader to load rust earlier

---
+++ quantum_boot/src/bios_boot/stage-1/rust/target/debug/build/stage-1-bf1a5bae585a9f90/build_script_build-bf1a5bae585a9f90.d
@@ -0,0 +1,5 @@
+/home/corigan01/CLionProjects/quantum_os/quantum_boot/src/bios_boot/stage-1/rust/target/debug/build/stage-1-bf1a5bae585a9f90/build_script_build-bf1a5bae585a9f90: build.rs
+
+/home/corigan01/CLionProjects/quantum_os/quantum_boot/src/bios_boot/stage-1/rust/target/debug/build/stage-1-bf1a5bae585a9f90/build_script_build-bf1a5bae585a9f90.d: build.rs
+
+build.rs:
