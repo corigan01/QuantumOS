35e4e9e09d19fa77fb244b639bc71a7535fcf706
Main Menu <corigan01@gmail.com>
2023-02-21 14:46:40 -0600

    bootloader

--- quantum_boot/src/bios_boot/stage-1/build/CMakeFiles/COMMAND.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/COMMAND"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/COMMAND.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
