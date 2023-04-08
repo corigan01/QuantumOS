627e2a3ab12bec264331c6fc6f22c51fd96d9f32
Main Menu <corigan01@gmail.com>
2023-04-08 00:46:53 -0500

    Quantum Builder: Remove dummy vector

--- bootloader/src/bios_boot/stage-1/src/main.rs
+++ bootloader/src/bios_boot/stage-1/src/main.rs
@@ -36,16 +36,16 @@ use quantum_lib::simple_allocator::SimpleBumpAllocator;
 
 use stage_1::bios_disk::BiosDisk;
 use stage_1::bios_ints::{BiosInt, TextModeColor};
+use stage_1::bios_println;
 use stage_1::config_parser::BootloaderConfig;
-use stage_1::error::BootloaderError;
 use stage_1::filesystem::FileSystem;
 use stage_1::vesa::{BiosVesa, Res};
-use stage_1::{bios_print, bios_println};
 
 global_asm!(include_str!("init.s"));
 
 #[no_mangle]
 extern "C" fn bit16_entry(disk_number: u16) {
+    bios_println!("\n --- Quantum Boot loader 16 ---\n");
     enter_rust(disk_number);
     panic!("Stage1 should not return!");
 }
@@ -53,7 +53,6 @@ extern "C" fn bit16_entry(disk_number: u16) {
 static mut TEMP_ALLOC: Option<SimpleBumpAllocator> = None;
 
 fn enter_rust(disk_id: u16) {
-    bios_println!("\n --- Quantum Boot loader 16 ---\n");
     let mut boot_info = BootInfo::default();
 
     unsafe {
@@ -71,15 +70,15 @@ fn enter_rust(disk_id: u16) {
                 .mount_root_if_contains("/bootloader/bootloader.cfg")
                 .expect("Could detect bootloader partition, please add \'/bootloader/bootloader.cfg\' to the bootloader filesystem for a proper boot!");
 
-        let bootloader_config_file =
+        let bootloader_config_file_ptr =
             unsafe { TEMP_ALLOC.as_mut().unwrap().allocate_region(256).unwrap() };
         let bootloader_filename = "/bootloader/bootloader.cfg";
 
-        fs.load_file_into_slice(bootloader_config_file, bootloader_filename)
+        fs.load_file_into_slice(bootloader_config_file_ptr, bootloader_filename)
             .expect("Unable to load bootloader config file!");
 
         bootloader_config =
-            BootloaderConfig::from_str(core::str::from_utf8(bootloader_config_file).unwrap())
+            BootloaderConfig::from_str(core::str::from_utf8(bootloader_config_file_ptr).unwrap())
                 .expect("Unable to parse bootloader config!");
 
         bios_println!("{:#?}", bootloader_config);
