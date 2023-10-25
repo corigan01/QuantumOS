76e52836f8adecda2e9abf1167e6ea4c7ecfdf94
gavinkellam <corigan01@ksu.com>
2023-10-25 17:00:10 -0500

    ATA PIO SectorRegister

--- lib/fs/src/disks/ata/registers/sector.rs
+++ lib/fs/src/disks/ata/registers/sector.rs
@@ -24,7 +24,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 */
 
 use super::{
-    DiskID, IOPortOffset, ReadRegisterBus, ResolveIOPortBusOffset, WriteRegisterBus,
+    DiskID, ReadRegisterBus, ResolveIOPortBusOffset, WriteRegisterBus,
     SECTOR_COUNT_OFFSET_FROM_IO_BASE, SECTOR_NUM_HIGH_OFFSET_FROM_IO_BASE,
     SECTOR_NUM_LOW_OFFSET_FROM_IO_BASE, SECTOR_NUM_MID_OFFSET_FROM_IO_BASE,
 };
@@ -52,3 +52,56 @@ unsafe impl WriteRegisterBus<SECTOR_COUNT_OFFSET_FROM_IO_BASE> for SectorRegiste
 unsafe impl WriteRegisterBus<SECTOR_NUM_LOW_OFFSET_FROM_IO_BASE> for SectorRegister<SectorLow> {}
 unsafe impl WriteRegisterBus<SECTOR_NUM_MID_OFFSET_FROM_IO_BASE> for SectorRegister<SectorMid> {}
 unsafe impl WriteRegisterBus<SECTOR_NUM_HIGH_OFFSET_FROM_IO_BASE> for SectorRegister<SectorHigh> {}
+
+impl SectorRegister {
+    pub unsafe fn zero_all_registers(disk: DiskID) {
+        SectorRegister::<SectorCount>::write(disk, 0);
+        SectorRegister::<SectorLow>::write(disk, 0);
+        SectorRegister::<SectorMid>::write(disk, 0);
+        SectorRegister::<SectorHigh>::write(disk, 0);
+    }
+
+    pub fn is_registers_zeroed(disk: DiskID) -> bool {
+        if SectorRegister::<SectorCount>::read(disk) != 0 {
+            return false;
+        }
+
+        if SectorRegister::<SectorLow>::read(disk) != 0 {
+            return false;
+        }
+
+        if SectorRegister::<SectorMid>::read(disk) != 0 {
+            return false;
+        }
+
+        if SectorRegister::<SectorHigh>::read(disk) != 0 {
+            return false;
+        }
+
+        true
+    }
+
+    /// # Set Sectors per Operation
+    /// Selects how many sectors the disk is going to process at one time. When reading/writting
+    /// this selects how many sectors its going to read/write.
+    pub fn set_sectors_per_operation(device: DiskID, sectors: u8) {
+        unsafe { SectorRegister::<SectorCount>::write(device, sectors) }
+    }
+
+    /// # LBA Bits 0 to 24
+    ///
+    /// # Panics
+    /// Asserts to ensure that only the lower 24 bits are set.
+    pub fn lba_bits_0_24(device: DiskID, lba_bits: usize) {
+        assert!(
+            lba_bits & 0xFFFFFF == lba_bits,
+            "LBA Bits should not be larger then 24 bits!"
+        );
+
+        unsafe {
+            SectorRegister::<SectorLow>::write(device, (lba_bits & 0xFF) as u8);
+            SectorRegister::<SectorMid>::write(device, ((lba_bits >> 8) & 0xFF) as u8);
+            SectorRegister::<SectorHigh>::write(device, ((lba_bits >> 16) & 0xFF) as u8);
+        }
+    }
+}
