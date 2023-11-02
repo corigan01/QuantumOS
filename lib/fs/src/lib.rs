73e9c909d44381d4a699e03b993b90f45cbdd76b
gavinkellam <corigan01@ksu.com>
2023-11-01 21:11:51 -0500

    FS: Added fs.rs to lib.rs

--- lib/fs/src/lib.rs
+++ lib/fs/src/lib.rs
@@ -26,17 +26,19 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #![no_std]
 #![feature(error_in_core)]
 #![feature(exclusive_range_pattern)]
-
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
 use crate::error::FsError;
 
-pub mod filesystems;
-pub mod io;
+mod abstract_buffer;
 pub mod disks;
 pub mod error;
-mod abstract_buffer;
+pub mod filesystems;
+pub mod io;
+
+mod fs;
+pub use crate::fs::*;
 
 pub type FsResult<T> = Result<T, FsError>;
 
