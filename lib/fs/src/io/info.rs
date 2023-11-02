7c00c3d3b3d0e2be85b5143d9dd78e70dc425015
gavinkellam <corigan01@ksu.com>
2023-11-01 21:07:16 -0500

    FS trait machine info

--- lib/fs/src/io/info.rs
+++ lib/fs/src/io/info.rs
@@ -21,4 +21,43 @@ NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPO
 NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
 */
+
+/// # Disk Info Provider
+/// Info about a disk, including the size, name, and machine name. Used by fs to show properties
+/// about disks.
+pub trait DiskInfoProvider {
+    fn human_name(&mut self) -> Option<String>;
+    fn machine_name(&mut self) -> Option<String>;
+
+    fn size_bytes(&mut self) -> Option<usize>;
+}
+
+/// # Machine Status Flags
+pub enum MachineStatusFlags {
+    /// # Ask Disconnect
+    /// Sets the status that this machine interface would like to disconnect from the host.
+    AskDisconnect,
+    /// # Forced Disconnect
+    /// This device was yanked out and is now disconnected. (Not ejecting a drive would set this
+    /// status)
+    ForcedDisconnect,
+    /// # Not Responding
+    /// The machine is not responding to commands, or has a child who is not responding.
+    NotResponding,
+    /// # Ask Connect
+    /// Asks for connection.
+    AskConnect,
+    /// # Ready
+    /// This machine is ready for commands
+    Ready,
+}
+
+/// # Machine Status Field Flags
+/// The flags that machine status info can return.
+pub struct MachineStatusFieldFlags(u64);
+
+/// # Machine Status Info
+/// Gather infomation about the machine.
+pub trait MachineStatusInfo {}
