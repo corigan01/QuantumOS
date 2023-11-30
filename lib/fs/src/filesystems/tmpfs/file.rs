767b52769d477b5de1317780a840e33b1aeab3f7
corigan01 <corigan01@gmail.com>
2023-11-29 21:04:13 -0600

    fixed seek build warning

--- lib/fs/src/filesystems/tmpfs/file.rs
+++ lib/fs/src/filesystems/tmpfs/file.rs
@@ -25,7 +25,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 
 use crate::{
     impl_seek,
-    io::{FileProvider, Metadata, Read, Seek, Write},
+    io::{FileProvider, Metadata, Read, Write},
     path::Path,
     permission::Permissions,
 };
