375e7aec20c78924092b6817d4019f057a69823a
corigan01 <corigan01@gmail.com>
2023-12-02 19:28:57 -0600

    path tests no longer passing

--- lib/fs/src/path.rs
+++ lib/fs/src/path.rs
@@ -71,101 +71,14 @@ impl Path {
     ///     desolved = "/"
     ///
     pub fn truncate_path(&self) -> Path {
-        let mut starting_path = self.0.clone();
-
-        // TODO: Make this better in the future
-        // I would like to do this without memory allocation in the future. I think memory
-        // allocation is just slowing this down.
-
-        let final_string: String = loop {
-            let before_buffer = [""].iter().map(|entry| *entry);
-            let mut final_string: String = before_buffer
-                .chain(
-                    Path::from(starting_path.clone())
-                        // Get the children of the path
-                        // Example path = "/home/user/someone"
-                        //  1. 'home'
-                        //  2. 'user'
-                        //  3. 'someone'
-                        .children()
-                        .into_iter()
-                        // Remove all '.'
-                        .filter(|child| child != &".")
-                        .chain(["", "", ""]),
-                )
-                .collect::<Vec<&str>>()
-                // Remove all "/path/../path/"
-                .windows(2)
-                .scan(0_usize, |val, init| {
-                    if *val > 0 {
-                        *val = val.checked_sub(1).unwrap_or(0);
-                        return Some("");
-                    }
-                    let first = &init[0];
-                    let second = &init[1];
-
-                    if second == &".." && first != &".." {
-                        *val += 1;
-                        Some("")
-                    } else {
-                        *val = val.checked_sub(1).unwrap_or(0);
-                        Some(first)
-                    }
-                })
-                .filter(|val| val.len() != 0)
-                // Add the '/' back for each of the children
-                .fold(
-                    starting_path
-                        .starts_with('/')
-                        .then(|| String::from("/"))
-                        .unwrap_or(String::new()),
-                    |mut acc, val| {
-                        acc.push_str(val);
-                        acc.push_str("/");
-
-                        acc
-                    },
-                );
-
-            // Replace some remaining truncated chars
-            if starting_path.starts_with(".") && !final_string.starts_with(".") {
-                final_string = String::from(".") + final_string;
-            }
-
-            if starting_path.ends_with("/") && !final_string.ends_with("/") {
-                final_string.push_str("/");
-            }
-
-            if !(starting_path.ends_with("/") || starting_path.ends_with("."))
-                && final_string.ends_with("/")
-            {
-                final_string.pop();
-            }
-
-            if starting_path.starts_with("/") && final_string.len() == 0 {
-                final_string.push_str("/");
-            }
-
-            if final_string.len() == 0 {
-                final_string.push_str(".");
-            }
-
-            if !final_string.contains("..") || final_string.starts_with("..") {
-                break final_string;
-            }
-
-            starting_path = final_string;
-        };
-
-        Path(final_string)
+        todo!()
     }
 
-    pub fn children<'a>(&'a self) -> Vec<&'a str> {
+    pub fn children<'a>(&'a self) -> impl Iterator<Item = &'a str> {
         self.0
             .as_str()
             .split('/')
-            .filter(|child| !(*child == "/") && !(*child == ""))
-            .collect()
+            .filter(|member| member.len() != 0)
     }
 
     pub fn is_absolute(&self) -> bool {
@@ -177,19 +90,7 @@ impl Path {
     }
 
     pub fn snip_off(self, path: Path) -> Option<Path> {
-        let mut path = path.truncate_path();
-
-        if path.0.ends_with("/") {
-            path = Path(path.0.as_str()[..path.0.len() - 1].into())
-        }
-
-        if !self.0.starts_with(path.as_str()) {
-            return None;
-        }
-
-        Some(Path::from(String::from(
-            &self.0.as_str()[path.as_str().len()..],
-        )))
+        todo!()
     }
 
     pub fn as_str<'a>(&'a self) -> &'a str {
@@ -197,40 +98,11 @@ impl Path {
     }
 
     pub fn is_child_of(&self, path: &Path) -> bool {
-        if self.0.starts_with(path.0.as_str()) {
-            return true;
-        }
-
-        let truncated_self = self.clone().truncate_path();
-        let truncated_other = path.clone().truncate_path();
-
-        truncated_self.0.starts_with(truncated_other.0.as_str())
+        todo!()
     }
 
     pub fn parent_path(&self) -> Path {
-        let parent_truncated = self.clone().truncate_path();
-        let mut children = parent_truncated.children();
-        children.pop();
-
-        if self.0.ends_with("/") && self.0.len() > 1 {
-            children.pop();
-        }
-
-        let mut rebuilt = String::new();
-        for child in children {
-            rebuilt.push_str(child);
-            rebuilt.push('/');
-        }
-
-        if !self.0.ends_with("/") {
-            rebuilt.pop();
-        }
-
-        if rebuilt.is_empty() {
-            rebuilt.push_str("/");
-        }
-
-        Path::from(rebuilt)
+        todo!()
     }
 }
 
