90a7a309add9385048ea6cc135aca0424b5e63cc
corigan01 <corigan01@gmail.com>
2024-01-28 13:32:13 -0600

    move gdt to gdt.rs

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -24,3 +24,48 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 */
 
 // GDT
+use core::{arch::asm, mem::size_of};
+
+static mut LONG_MODE_GDT: GdtLongMode = GdtLongMode::new();
+
+#[repr(C)]
+pub struct GdtLongMode {
+    zero: u64,
+    code: u64,
+    data: u64,
+}
+
+impl GdtLongMode {
+    const fn new() -> Self {
+        let common_flags = {
+            (1 << 44) // user segment
+                | (1 << 47) // present
+                | (1 << 41) // writable
+                | (1 << 40) // accessed (to avoid changes by the CPU)
+        };
+        Self {
+            zero: 0,
+            code: common_flags | (1 << 43) | (1 << 53), // executable and long mode
+            data: common_flags,
+        }
+    }
+
+    pub fn load(&'static self) {
+        let pointer = GdtPointer {
+            limit: (3 * size_of::<u64>() - 1) as u16,
+            base: self,
+        };
+
+        unsafe {
+            asm!("lgdt [{}]", in(reg) &pointer);
+        }
+    }
+}
+
+#[repr(C, packed(2))]
+pub struct GdtPointer {
+    /// Size of the DT.
+    pub limit: u16,
+    /// Pointer to the memory region containing the DT.
+    pub base: *const GdtLongMode,
+}
