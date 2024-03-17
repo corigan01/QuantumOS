cce7cd10c100dec7b6005f7f58c432a5bbf1bd58
corigan01 <corigan01@gmail.com>
2024-03-17 15:17:07 -0500

    Bootloader: Disk DAP read function
    Reading bios disk with DAP

--- bootloader/stage-bootsector/src/disk.rs
+++ bootloader/stage-bootsector/src/disk.rs
@@ -8,4 +8,38 @@ struct DiskAccessPacket {
     lba: u64,
 }
 
-impl DiskAccessPacket {}
+impl DiskAccessPacket {
+    pub fn read(disk: u8, sectors: u16, lba: u64, ptr: (u16, u16)) {
+        let base_segment = ptr.0;
+        let base_ptr = ptr.1;
+
+        let packet = Self {
+            packet_size: core::mem::size_of::<Self>() as u8,
+            always_zero: 0,
+            sectors,
+            base_ptr,
+            base_segment,
+            lba,
+        };
+
+        let packet_address = &packet as *const Self;
+
+        unsafe {
+            core::arch::asm!("
+                push si
+                mov si, {packet:x}
+                int 0x13
+                push ax
+                mov al, 'D'
+                jc .fail
+                pop ax
+                pop si
+                pop si
+            ",
+                packet = in(reg) packet_address,
+                in("dl") disk,
+                in("ax") 0x4200,
+            )
+        };
+    }
+}
