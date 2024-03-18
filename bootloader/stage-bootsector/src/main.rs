6bd066bdb1782348f7379d7a83d5920c75ddd336
corigan01 <corigan01@gmail.com>
2024-03-17 21:25:18 -0500

    Bootloader: Loading sectors from mbr into memory

--- bootloader/stage-bootsector/src/main.rs
+++ bootloader/stage-bootsector/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 mod disk;
+mod partition;
 mod tiny_panic;
 
 use core::{arch::global_asm, include_str};
@@ -10,7 +11,18 @@ global_asm!(include_str!("init.s"));
 
 #[no_mangle]
 extern "C" fn main(disk_number: u16) {
-    let dap = disk::DiskAccessPacket::new(1, 0, 0x0800);
-    dap.read(disk_number);
+    let bootable = unsafe { &mut *partition::find_bootable() };
+
+    loop {
+        disk::DiskAccessPacket::new(1, bootable.lba as u64, 0x7E00).read(disk_number);
+
+        bootable.lba += 1;
+        bootable.count -= 1;
+
+        if bootable.count <= 0 {
+            break;
+        }
+    }
+
     tiny_panic::fail(b'&');
 }
