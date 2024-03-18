427f1b37d681e2d228dc25af7a40a6c497b35b51
corigan01 <corigan01@gmail.com>
2024-03-18 11:45:28 -0500

    Bootloader: Tmp Build script now works with new building

--- tmp_build.sh
+++ tmp_build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd bootloader/stage-bootsector/
-cargo clean && cargo build --release --target ../../linkerscripts/i386-quantum_loader.json
-cd ../../target/i386-quantum_loader/release/
-ls
-objcopy -I elf32-i386 -O binary ./stage-bootsector && hexdump stage-bootsector && qemu-system-x86_64 -drive format=raw,file=./stage-bootsector  -enable-kvm
+cd bootloader/
+cargo build
+qemu-system-x86_64 -drive format=raw,file=./target/i386-quantum_loader/stage-bootsector/stage-bootsector.bin  -enable-kvm
