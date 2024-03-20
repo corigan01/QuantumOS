b88d62e80d1099b8645ac4827dcec7a4f2b59827
corigan01 <corigan01@gmail.com>
2024-03-20 14:17:11 -0500

    Meta: Spawning Emulator to boot OS

--- meta/src/main.rs
+++ meta/src/main.rs
@@ -1,10 +1,12 @@
 #![feature(async_closure)]
 #![feature(async_fn_traits)]
 
-use anyhow::{Context, Error, Result};
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use futures::executor::block_on;
-use std::path::Path;
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 use crate::{artifacts::build_project, disk::DiskImgBaker};
 
@@ -12,7 +14,7 @@ mod artifacts;
 mod cmdline;
 mod disk;
 
-async fn build() -> Result<()> {
+async fn build() -> Result<PathBuf> {
     let artifacts = build_project().await.context("Failed to build artifacts")?;
     println!("{:#?}", artifacts);
 
@@ -20,7 +22,35 @@ async fn build() -> Result<()> {
     disk.write_bootsector(&artifacts.bootsector).await?;
     disk.write_stage16(&artifacts.stage_16).await?;
 
-    disk.finish_and_write().await?;
+    disk.finish_and_write().await
+}
+
+fn run_qemu(disk_target_path: &Path) -> Result<()> {
+    Command::new("qemu-system-x86_64")
+        .arg("-device")
+        .arg("isa-debug-exit,iobase=0xf4,iosize=0x04")
+        .arg("-d")
+        //        .arg("int")
+        .arg("cpu_reset")
+        .arg("--no-reboot")
+        .arg("-m")
+        .arg("256M")
+        .arg("-k")
+        .arg("en-us")
+        .arg("-nic")
+        .arg("none")
+        .arg("-drive")
+        .arg(format!(
+            "format=raw,file={}",
+            disk_target_path.to_str().unwrap()
+        ))
+        .stdout(std::process::Stdio::inherit())
+        .status()
+        .context(anyhow!("Could not start qemu-system-x86_64!"))?
+        .success()
+        .then_some(())
+        .ok_or(anyhow!("QEMU Failed"))?;
+
     Ok(())
 }
 
@@ -34,7 +64,7 @@ async fn main() -> Result<()> {
             todo!("build")
         }
         cmdline::TaskOption::Run => {
-            build().await?;
+            run_qemu(&build().await?)?;
         }
         cmdline::TaskOption::Clean => {
             todo!("clean")
