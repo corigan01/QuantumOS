216e28ef4c766bc7d14e688b7513416e90ab6b28
corigan01 <corigan01@gmail.com>
2024-03-20 18:52:46 -0500

    Bootloader: Enter unreal mode in 16bit-full

--- bootloader/stage-16bit/src/main.rs
+++ bootloader/stage-16bit/src/main.rs
@@ -1,15 +1,20 @@
 #![no_std]
 #![no_main]
 
+use unreal::enter_unreal;
+
 mod panic;
+mod unreal;
 
 #[no_mangle]
 #[link_section = ".begin"]
 extern "C" fn entry(disk_id: u16) {
+    unsafe { enter_unreal() };
+
     for c in b"test" {
         panic::putc(*c);
     }
-    // panic!("Panic!");
+    panic!("Test");
 
     loop {}
 }
