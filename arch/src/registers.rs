c1f68c2af16501f53ec1c606935f41d897259c07
corigan01 <corigan01@gmail.com>
2024-03-21 12:36:54 -0500

    Arch: Added x86 GP registers to arch

--- arch/src/registers.rs
+++ arch/src/registers.rs
@@ -1,1 +1,55 @@
+#[repr(C)]
+pub struct Regs8 {
+    pub al: u8,
+    pub ah: u8,
+    pub bl: u8,
+    pub bh: u8,
+    pub cl: u8,
+    pub ch: u8,
+    pub dl: u8,
+    pub dh: u8,
+}
 
+#[repr(C)]
+pub struct Regs16 {
+    pub ax: u16,
+    pub bx: u16,
+    pub cx: u16,
+    pub dx: u16,
+    pub bp: u16,
+    pub sp: u16,
+    pub si: u16,
+    pub di: u16,
+}
+
+#[repr(C)]
+pub struct Regs32 {
+    pub eax: u32,
+    pub ebx: u32,
+    pub ecx: u32,
+    pub edx: u32,
+    pub ebp: u32,
+    pub esp: u32,
+    pub esi: u32,
+    pub edi: u32,
+}
+
+#[repr(C)]
+pub struct Regs64 {
+    pub rax: u64,
+    pub rbx: u64,
+    pub rcx: u64,
+    pub rdx: u64,
+    pub rsi: u64,
+    pub rdi: u64,
+    pub rbp: u64,
+    pub rsp: u64,
+    pub r8: u64,
+    pub r9: u64,
+    pub r10: u64,
+    pub r11: u64,
+    pub r12: u64,
+    pub r13: u64,
+    pub r14: u64,
+    pub r15: u64,
+}
