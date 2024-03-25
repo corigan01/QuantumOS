3ea3173097a7719c9df7531fa4a48833dfefa25e
corigan01 <corigan01@gmail.com>
2024-03-25 12:37:01 -0500

    Bootloader: Inital Support for reading Fat disk

--- bootloader/stage-16bit/src/fatfs/bpb.rs
+++ bootloader/stage-16bit/src/fatfs/bpb.rs
@@ -1,4 +1,24 @@
-use super::FatKind;
+use super::{FatKind, ReadSeek};
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct Bpb {
+    jmp_boot: [u8; 3],
+    oem_name: [u8; 8],
+    bytes_per_sector: u16,
+    sectors_per_cluster: u8,
+    reserved_sectors: u16,
+    number_fats: u8,
+    root_entries: u16,
+    total_sectors_fat16: u16,
+    media_type: u8,
+    fat_sectors_fat16: u16,
+    sectors_per_track: u16,
+    head_count: u16,
+    hidden_sectors: u32,
+    total_sectors_fat32: u32,
+    extended: ExtendedBpb,
+}
 
 #[repr(C)]
 #[derive(Clone, Copy)]
@@ -36,26 +56,6 @@ union ExtendedBpb {
     fat32: Bpb32,
 }
 
-#[repr(C)]
-#[derive(Clone, Copy)]
-pub struct Bpb {
-    jmp_boot: [u8; 3],
-    oem_name: [u8; 8],
-    bytes_per_sector: u16,
-    sectors_per_cluster: u8,
-    reserved_sectors: u16,
-    number_fats: u8,
-    root_entries: u16,
-    total_sectors_fat16: u16,
-    media_type: u8,
-    fat_sectors_fat16: u16,
-    sectors_per_track: u16,
-    head_count: u16,
-    hidden_sectors: u32,
-    total_sectors_fat32: u32,
-    extended: ExtendedBpb,
-}
-
 enum ExtendedKind<'a> {
     Fat16(&'a Bpb16),
     Fat32(&'a Bpb32),
@@ -65,6 +65,28 @@ impl Bpb {
     const ROOT_ENTRY_SIZE: usize = 32;
     const FAT12_CLUSTERS: usize = 4085;
     const FAT16_CLUSTERS: usize = 65525;
+
+    pub(crate) fn new<Disk: ReadSeek>(disk: &mut Disk) -> Result<Self, &'static str> {
+        let mut bpb = unsafe { core::mem::zeroed::<Self>() };
+        disk.seek(0);
+
+        // Treat ourself as if we were a slice
+        let self_slice = unsafe {
+            &mut (*core::ptr::slice_from_raw_parts_mut(
+                ((&mut bpb) as *mut Self) as *mut u8,
+                core::mem::size_of_val(&bpb),
+            ))
+        };
+
+        disk.read(self_slice);
+
+        // TODO: Add more checks for BPB to ensure that it is valid before returning it
+        if bpb.bytes_per_sector == 0 || bpb.sectors_per_cluster == 0 {
+            return Err("Not valid BPB structure on disk");
+        }
+
+        Ok(bpb)
+    }
 
     fn root_sectors(&self) -> usize {
         // 3.5 Determination of FAT type when mounting the Volume (page: 14)
--- bootloader/stage-16bit/src/fatfs/mod.rs
+++ bootloader/stage-16bit/src/fatfs/mod.rs
@@ -1,7 +1,26 @@
+use self::bpb::Bpb;
+use crate::io::{Read, Seek};
+
 mod bpb;
 
 pub enum FatKind {
     Fat12,
     Fat16,
     Fat32,
+}
+
+pub(super) trait ReadSeek: Read + Seek {}
+impl<T: Read + Seek> ReadSeek for T {}
+
+pub struct Fat<Part: ReadSeek> {
+    disk: Part,
+    bpb: Bpb,
+}
+
+impl<Part: ReadSeek> Fat<Part> {
+    pub fn new(mut disk: Part) -> Result<Self, &'static str> {
+        let bpb = Bpb::new(&mut disk)?;
+
+        Ok(Self { disk, bpb })
+    }
 }
