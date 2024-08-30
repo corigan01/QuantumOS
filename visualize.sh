304f6c3c5510efc39a2754832095659a070d394e
corigan01 <corigan01@gmail.com>
2024-08-30 13:13:26 -0500

    Visualize: Change the name of the project and speed it up

--- visualize.sh
+++ visualize.sh
@@ -1,1 +1,1 @@
-gource -1920x1080 --title "MAX78000" --hide progress --max-user-speed 500 --seconds-per-day 0.50 -e 0.005 --bloom-intensity 0.05 --hide filenames
+gource -1920x1080 --title "QuantumOS" --hide progress --max-user-speed 700 --seconds-per-day 0.25 -e 0.005 --bloom-intensity 0.05 --hide filenames
