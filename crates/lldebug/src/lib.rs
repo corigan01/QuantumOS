8503bc3ca03418ea5d0378e84c497cae61ef1ae3
corigan01 <corigan01@gmail.com>
2024-09-08 02:05:56 -0500

    LLDebug: Do init work on debug stream and formatting for structs

--- crates/lldebug/src/lib.rs
+++ crates/lldebug/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+
+use core::fmt::Write;
+
+/// # Instance String
+/// The name used as a header for each newline printed. Useful for describing where
+/// in the operating system you are executing.
+static mut INSTANCE_STRING: Option<&'static str> = None;
+
+/// # Set Crate Nametag
+/// This function will tell LLDebug who you are, and will tell the user that this
+/// output is coming from your crate.
+pub fn set_crate_nametag(name: &'static str) {
+    unsafe { INSTANCE_STRING = Some(name) };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// # Outlet Ref
+/// The type of a rust `core::fmt::Write`-able struct for sending out debug
+/// strings to the user.
+pub type OutletRef = &'static mut (dyn Write + Sync + Send);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// # Debug Stream
+/// Debug stream describes an output to which debug info can be posted. For
+/// example, you can attach a serial debug and vga debug stream and have all
+/// debug output routed to both.
+pub struct DebugStream {
+    stream_name: &'static str,
+    outlet: OutletRef,
+}
+
+impl DebugStream {
+    /// # New
+    /// Create a new debug stream.
+    pub fn new(stream_name: &'static str, outlet: OutletRef) -> Self {
+        Self {
+            stream_name,
+            outlet,
+        }
+    }
+}
+
+impl Write for DebugStream {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        todo!()
     }
 }
