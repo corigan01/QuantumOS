9769a47929883f2fef99c7da1d2bb93f1e1d4976
corigan01 <corigan01@gmail.com>
2024-09-09 22:02:11 -0500

    LLdebug: Init support for output stream support

--- crates/lldebug/src/lib.rs
+++ crates/lldebug/src/lib.rs
@@ -7,6 +7,10 @@ use core::fmt::Write;
 /// in the operating system you are executing.
 static mut INSTANCE_STRING: Option<&'static str> = None;
 
+/// # Output Streams
+/// Streams that are ready to output data.
+static mut OUTPUT_STREAMS: [Option<DebugStream>; 4] = [None, None, None, None];
+
 /// # Set Crate Nametag
 /// This function will tell LLDebug who you are, and will tell the user that this
 /// output is coming from your crate.
@@ -37,10 +41,66 @@ impl DebugStream {
             outlet,
         }
     }
+
+    /// # Post
+    /// Upload this stream to the global list of output stream. This
+    /// stream will now start getting debug output.
+    pub fn post(self) {
+        if let Some(output) = unsafe { OUTPUT_STREAMS.iter_mut().find(|stream| stream.is_some()) } {
+            *output = Some(self);
+        } else {
+            println!("Tried to add a stream, but stream list is full!");
+            _ = self.outlet.write_fmt(format_args!(
+                "Tried to add this stream to already full stream list!"
+            ));
+        }
+    }
 }
 
 impl Write for DebugStream {
     fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        todo!()
+        for c in s.chars() {
+            match c {
+                '\n' => self.outlet.write_fmt(format_args!(
+                    "[{}->{}]:",
+                    unsafe { INSTANCE_STRING.unwrap_or("UNKNOWN") },
+                    self.stream_name
+                ))?,
+                c => self.outlet.write_char(c)?,
+            }
+        }
+
+        Ok(())
     }
+}
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    unsafe {
+        OUTPUT_STREAMS.iter_mut().for_each(|stream| {
+            if let Some(stream) = stream {
+                _ = stream.outlet.write_fmt(args);
+            }
+        })
+    }
+}
+
+/// # Print
+/// Output to global output stream.
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {{
+        $crate::_print(format_args!($($arg)*));
+    }};
+}
+
+/// # Println
+/// Output to global output stream.
+#[macro_export]
+macro_rules! println {
+    () => ($crate::debug_print!("\n"));
+    ($($arg:tt)*) => {{
+        $crate::_print(format_args!($($arg)*));
+        $crate::print!("\n");
+    }}
 }
