f3fbb577bca0f587a00c272dbe312c4c21afec15
corigan01 <corigan01@gmail.com>
2024-10-15 16:34:53 -0500

    lldebug: Move tests into lldebug

---
+++ crates/lldebug/tests/one/one.rs
@@ -0,0 +1,9 @@
+use lldebug_macro::make_debug;
+
+make_debug! {
+    /// # Debug Thing
+    /// This is a thing that is a thing.
+    Debug: Option<i32> = { Some(0) };
+}
+
+fn main() {}
---
+++ crates/lldebug/tests/two/two.rs
@@ -0,0 +1,8 @@
+use lldebug_macro::make_debug;
+
+make_debug! {
+    Debug: i32 = 0;
+    Debug: i32 = 0;
+}
+
+fn main() {}
