a782830c924dc1faf7428fa89cdc9d698fe68600
corigan01 <corigan01@gmail.com>
2024-10-22 20:29:27 -0500

    Paging: Add a paging macro to simplify making many page tables

--- crates/arch/src/paging.rs
+++ crates/arch/src/paging.rs
@@ -23,36 +23,9 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-/// A Page Directry Entry field.
-///
-/// # How to use?
-/// For Example, one might:
-/// ```rust
-/// use arch::paging::PageDirectryEntry;
-///
-/// let entry = PageDirectryEntry::empty()
-///     .present(true)
-///     .read_write(true)
-///     .user_access(true);
-/// ```
-///
-/// Here we are building a `PageDirectryEntry` with the `P`, `R/W`, and `U/S` bits set. The
-/// bit-field in `entry` will correspond to this change (should be compiled in).
-///
-/// # Safety
-/// This is not 'unsafe however, its not fully 'safe' either. When loading the page
-/// tables themselves, one must understand and verify that all page tables are
-/// loaded correctly. Each entry in the page table isn't unsafe by itself,
-/// however, when loaded into the system it becomes unsafe.
-///
-/// It would be a good idea to verify that all 'bit' or options set in this entry  does exactly
-/// what you intend it to do before loading it. Page tables can cause the entire system to become
-/// unstable if mapped wrong -- **this is very important.**
-#[derive(Clone, Copy)]
-pub struct PageDirectryEntry(u64);
-
 macro_rules! impl_bit {
-    {$name:ident, $bit:literal $(#[$meta:meta])*} => {
+    // TODO: Make a proc macro for doing this bit stuff in a more general way for more things to use it
+    {$name:ident, $get_name: ident, $bit:literal $(#[$meta:meta])*} => {
         $(#[$meta])*
         /// # Important Considerations
         /// This function is `const` and will be best used when setup in 'chains' at compile time.
@@ -75,75 +48,145 @@ macro_rules! impl_bit {
 
             *self
         }
+
+        // FIXME: Fix these docs
+        /// # The 'get' version of the docs below.
+        ///
+        /// ---
+        $(#[$meta])*
+        /// # Important Considerations
+        /// This function is `const` and will be best used when setup in 'chains' at compile time.
+        ///
+        /// # Safety
+        /// This is not 'unsafe however, its not fully 'safe' either. When loading the page
+        /// tables themselves, one must understand and verify that all page tables are
+        /// loaded correctly. Each entry in the page table isn't unsafe by itself,
+        /// however, when loaded into the system it becomes unsafe.
+        ///
+        /// It would be a good idea to verify that this 'bit' or option does exactly what you
+        /// intend it to do before loading it. Page tables can cause the entire system to become
+        /// unstable if mapped wrong -- **this is very important.**
+        pub const fn $get_name(&self) -> bool {
+            ((self.0 >> $bit) & 0b1) != 0
+        }
     };
 }
 
-impl PageDirectryEntry {
-    /// Make a completly blank page table entry.
-    ///
-    /// None of the bits are set.
-    pub const fn empty() -> Self {
-        Self(0)
-    }
+macro_rules! impl_entry {
+    ($($(#[$meta:meta])* $entry_name: ident),*) => {
+        $(
+        /// # How to use?
+        /// For Example, one might:
+        /// ```rust
+        /// use arch::paging::PageDirectryEntry;
+        ///
+        /// let entry = PageDirectryEntry::empty()
+        ///     .present(true)
+        ///     .read_write(true)
+        ///     .user_access(true);
+        /// ```
+        ///
+        /// Here we are building a `PageDirectryEntry` with the `P`, `R/W`, and `U/S` bits set. The
+        /// bit-field in `entry` will correspond to this change (should be compiled in).
+        ///
+        /// # Safety
+        /// This is not 'unsafe however, its not fully 'safe' either. When loading the page
+        /// tables themselves, one must understand and verify that all page tables are
+        /// loaded correctly. Each entry in the page table isn't unsafe by itself,
+        /// however, when loaded into the system it becomes unsafe.
+        ///
+        /// It would be a good idea to verify that all 'bit' or options set in this entry  does exactly
+        /// what you intend it to do before loading it. Page tables can cause the entire system to become
+        /// unstable if mapped wrong -- **this is very important.**
+        #[derive(Clone, Copy)]
+        pub struct $entry_name(u64);
 
-    impl_bit! {present, 0
-        /// Set the 'present' page table bit
-        ///
-        /// This bit determines if the page entry is 'active' or 'inactive The
-        /// CPU on x86 systems will always generate a #PF (Page Fault Exception)
-        /// when this bit is not set regardless of the rest of the page table's
-        /// state.
-        ///
-    }
-    impl_bit! {read_write, 1
-        /// Set the 'read/write' page table bit
-        ///
-        /// This bit determines if the page entry is able to be written to. In
-        /// its default state, the page is `RO` (Read Only). When setting this
-        /// bit to `true` it will allow access to `RW` (Read/Write) to the memory
-        /// pointed to by this page entry.
-    }
-    impl_bit! {user_access, 2
-        /// Set the 'superuser/user' page table bit.
-        ///
-        /// This bit determines if the page table is accessable from userspace.
-        /// In its default state, page entries are **not** accessable from userspace,
-        /// and will cause a #PF (Page Fault Exception).
-    }
-    impl_bit! {write_though, 3
-        /// Set the 'write though' page table bit.
-        ///
-        /// This bit determains if this page table entry is able to do write though
-        /// caching.
-    }
-    impl_bit! {cache_disable, 4
-        /// Set the 'cach disable' page table bit.
-        ///
-        /// This bit determains if this page table entry is able to be cahced or not,
-        /// when disabled this page table is never cached.
-    }
-    impl_bit! {accessed, 5
-        /// Set the 'accessed' page table bit.
-        ///
-        /// This bit determains if the CPU has written to this page table or not,
-        /// often called the dirty bit.
-    }
-    impl_bit! {page_size, 6
-        /// Set the 'page size' page table bit.
-        ///
-        /// This bit determains if the page table entry is the lowest entry in the
-        /// mapping, and does not point to further down page tables. Often used to
-        /// map large sections of memory instead of 4kib sections.
-        ///
-        /// # Note
-        /// This `page size` bit is only allowed on supported systems, and should
-        /// only be set on compoatable page table entries. If this page table entry
-        /// is not supported, the CPU will generate a #PF (Page Fault Excpetion) of
-        /// a 'reserved' value.
-    }
-    impl_bit! {execute_disable, 7
-        /// Set the 'execute disable' page table bit.
-        ///
-        /// This bit determains if CPU execution is allowed in this area.
-    }
+        impl $entry_name {
+            /// Make a completly blank page table entry.
+            ///
+            /// None of the bits are set.
+            pub const fn empty() -> Self {
+                Self(0)
+            }
+
+            impl_bit! {present, get_present, 0
+                /// Set the 'present' page table bit
+                ///
+                /// This bit determines if the page entry is 'active' or 'inactive The
+                /// CPU on x86 systems will always generate a #PF (Page Fault Exception)
+                /// when this bit is not set regardless of the rest of the page table's
+                /// state.
+                ///
+            }
+            impl_bit! {read_write, get_read_write, 1
+                /// Set the 'read/write' page table bit
+                ///
+                /// This bit determines if the page entry is able to be written to. In
+                /// its default state, the page is `RO` (Read Only). When setting this
+                /// bit to `true` it will allow access to `RW` (Read/Write) to the memory
+                /// pointed to by this page entry.
+            }
+            impl_bit! {user_access, get_user_access, 2
+                /// Set the 'superuser/user' page table bit.
+                ///
+                /// This bit determines if the page table is accessable from userspace.
+                /// In its default state, page entries are **not** accessable from userspace,
+                /// and will cause a #PF (Page Fault Exception).
+            }
+            impl_bit! {write_though, get_write_though, 3
+                /// Set the 'write though' page table bit.
+                ///
+                /// This bit determains if this page table entry is able to do write though
+                /// caching.
+            }
+            impl_bit! {cache_disable, get_cache_disable, 4
+                /// Set the 'cach disable' page table bit.
+                ///
+                /// This bit determains if this page table entry is able to be cahced or not,
+                /// when disabled this page table is never cached.
+            }
+            impl_bit! {accessed, get_accessed, 5
+                /// Set the 'accessed' page table bit.
+                ///
+                /// This bit determains if the CPU has read from the memory to which this page
+                /// points or not.
+            }
+            impl_bit! {page_size, get_page_size, 6
+                /// Set the 'page size' page table bit.
+                ///
+                /// This bit determains if the page table entry is the lowest entry in the
+                /// mapping, and does not point to further down page tables. Often used to
+                /// map large sections of memory instead of 4kib sections.
+                ///
+                /// # Note
+                /// This `page size` bit is only allowed on supported systems, and should
+                /// only be set on compoatable page table entries. If this page table entry
+                /// is not supported, the CPU will generate a #PF (Page Fault Excpetion) of
+                /// a 'reserved' value.
+            }
+            impl_bit! {execute_disable, get_execute_disable, 7
+                /// Set the 'execute disable' page table bit.
+                ///
+                /// This bit determains if CPU execution is allowed in this area.
+            }
+        })*
+    };
 }
+
+impl_entry!(
+    /// A Page Table Entry
+    ///
+    PageEntryLvl1,
+    /// A Page Directry Entry field.
+    ///
+    PageEntryLvl2,
+    /// A Page Directry Pointer Entry field.
+    ///
+    PageEntryLvl3,
+    /// A Page Map Level 4 Entry.
+    ///
+    PageEntryLvl4,
+    /// A Page Map Level 5 Entry.
+    ///
+    PageEntryLvl5
+);
