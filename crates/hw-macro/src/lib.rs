293f5f8a0a518f172a0e1e4ce9c857b935d06873
corigan01 <corigan01@gmail.com>
2024-10-29 11:42:55 -0500

    Hw: Setup crates to be proc macro

--- crates/hw-macro/src/lib.rs
+++ crates/hw-macro/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use proc_macro::TokenStream;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro]
+pub fn hw_device(tokens: TokenStream) -> TokenStream {
+    todo!()
 }
