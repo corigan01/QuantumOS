dfe794e60f4f227822e7d6a2af2a6efbb43fc769
corigan01 <corigan01@gmail.com>
2024-11-17 18:42:32 -0600

    Hw: Include program file header

--- crates/hw-macro/src/provider_parse.rs
+++ crates/hw-macro/src/provider_parse.rs
@@ -1,9 +1,33 @@
+/*
+  ____                 __               __   _ __
+ / __ \__ _____ ____  / /___ ____ _    / /  (_) /
+/ /_/ / // / _ `/ _ \/ __/ // /  ' \  / /__/ / _ \
+\___\_\_,_/\_,_/_//_/\__/\_,_/_/_/_/ /____/_/_.__/
+    Part of the Quantum OS Project
+
+Copyright 2024 Gavin Kellam
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{
-    parse::Parse,
     visit::{self, Visit},
-    Field, ItemMod, ItemStruct, Type,
+    Field, ItemFn, ItemMod, ItemStruct, Type,
 };
 
 use crate::make_hw_parse::{BitFieldType, MakeHwMacroInput};
@@ -196,11 +220,27 @@ impl<'a> GenWrite for MacroMod<'a> {
 }
 
 impl<'b> MacroMod<'b> {
+    pub fn get_read_function(&self) -> Option<&'b ItemFn> {
+        todo!()
+    }
+
+    pub fn get_write_function(&self) -> Option<&'b ItemFn> {
+        todo!()
+    }
+
     pub fn gen_reading<'a>(&'a self) -> Option<&'a dyn GenRead> {
-        Some(self)
+        if self.get_read_function().is_some() {
+            Some(self)
+        } else {
+            None
+        }
     }
 
     pub fn gen_writing<'a>(&'a self) -> Option<&'a dyn GenWrite> {
-        Some(self)
+        if self.get_write_function().is_some() {
+            Some(self)
+        } else {
+            None
+        }
     }
 }
